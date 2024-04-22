1679c1088e7b5a5bf1d0086ad79c866a00c41f36
Ecssooo <alexandre.lejeune9@gmail.com>
2024-04-22 11:05:40 +0200

    v1

---
+++ Assets/SSL/Runtime/Scripts/Hero/HeroController.cs
@@ -0,0 +1,42 @@
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+
+public class HeroController : MonoBehaviour
+{
+    [Header("Entity")]
+    [SerializeField] private HeroEntity _entity;
+
+    [Header("Debug")]
+    [SerializeField] private bool _guiDebug = false;
+
+    private void OnGUI()
+    {
+        if (!_guiDebug) return;
+
+        GUILayout.BeginVertical(GUI.skin.box);
+        GUILayout.Label(gameObject.name);
+        GUILayout.EndVertical();
+    }
+
+    private void Update()
+    {
+        _entity.SetMoveDirX(GetInputMoxeX());
+    }
+
+    private float GetInputMoxeX()
+    {
+        float inputMoveX = 0f;
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.Q))
+        {
+            inputMoveX = -1f;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputMoveX = 1f;
+        }
+
+        return inputMoveX;
+    }
+}
