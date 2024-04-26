dd368c850b746be2fb6733e99711c82f5e549bd4
Ecssooo <alexandre.lejeune9@gmail.com>
2024-04-26 11:02:05 +0200

    camera profile

--- Assets/SSL/Runtime/Scripts/Camera/CameraChangeProfileTrigger.cs
+++ Assets/SSL/Runtime/Scripts/Camera/CameraChangeProfileTrigger.cs
@@ -1,13 +1,34 @@
-﻿using UnityEngine;
+﻿using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
 
 public class CameraChangeProfileTrigger : MonoBehaviour
 {
+    private const string TARGET_TAG = "CameraTriggerTarget";
+    
     [Header("Camera Profile")]
     [SerializeField] private CameraProfile _cameraProfile;
     
     [Header("Trigger Box")]
     [SerializeField] private BoxCollider2D _boxCollider;
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag(TARGET_TAG)) return;
+        CameraManager.Instance.EnterProfile(_cameraProfile);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.CompareTag(TARGET_TAG)) return;
+        CameraManager.Instance.ExitProfile(_cameraProfile);
+    }
+    
+    
+    
+    
+    
+    
+    
     private void OnDrawGizmos()
     {
         if (_boxCollider == null) return;
--- Assets/SSL/Runtime/Scripts/Camera/CameraProfile.cs
+++ Assets/SSL/Runtime/Scripts/Camera/CameraProfile.cs
@@ -14,4 +14,3 @@ public class CameraProfile : MonoBehaviour
             _camera.enabled = false;
     }
 }
-
